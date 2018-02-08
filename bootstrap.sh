bcb826719d5d551d03c221ef8d72a8aeed16ce04
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-02-07 18:13:31 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 echo "Symlinking files from $(pwd)"
-ln -s $(pwd)/.vimrc ~/.config/nvim/init.vim
-ln -s $(pwd)/.exports ~/.config/.exports
-ln -s $(pwd)/.aliases ~/.config/.aliases
-ln -s $(pwd)/.tmux.conf ~/.tmux.conf
-ln -s $(pwd)/.bash_profile ~/.bash_profile
-ln -s $(pwd)/.zshrc ~/.zshrc
-ln -s $(pwd)/.condarc ~/.condarc
-ln -s $(pwd)/.yapf/style ~/.config/yapf/style
-ln -s $(pwd)/.flake8 ~/.config/flake8
-ln -s $(pwd)/.fzf.bash ~/.config/.fzf.bash
-ln -s $(pwd)/.fzf.zsh ~/.config/.fzf.zsh
-ln -s $(pwd)/.gdbinit ~/.gdbinit
-ln -s $(pwd)/.gitignore ~/.gitignore
-ln -s $(pwd)/.pylintrc ~/.pylintrc
+rm ~/.config/nvim/init.vim && ln -s $(pwd)/.vimrc ~/.config/nvim/init.vim
+rm ~/.config/.exports && ln -s $(pwd)/.exports ~/.config/.exports
+rm ~/.config/.aliases && ln -s $(pwd)/.aliases ~/.config/.aliases
+rm ~/.tmux.conf && ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+rm ~/.bash_profile && ln -s $(pwd)/.bash_profile ~/.bash_profile
+rm ~/.zshrc && ln -s $(pwd)/.zshrc ~/.zshrc
+rm ~/.condarc && ln -s $(pwd)/.condarc ~/.condarc
+rm ~/.config/yapf/style && ln -s $(pwd)/.yapf/style ~/.config/yapf/style
+rm ~/.config/flake8 && ln -s $(pwd)/.flake8 ~/.config/flake8
+rm ~/.config/.fzf.bash && ln -s $(pwd)/.fzf.bash ~/.config/.fzf.bash
+rm ~/.config/.fzf.zsh && ln -s $(pwd)/.fzf.zsh ~/.config/.fzf.zsh
+rm ~/.gdbinit && ln -s $(pwd)/.gdbinit ~/.gdbinit
+rm ~/.gitignore && ln -s $(pwd)/.gitignore ~/.gitignore
+rm ~/.pylintrc && ln -s $(pwd)/.pylintrc ~/.pylintrc
