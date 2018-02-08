1b3a4c2139e47ce3467ffd5abd695119b831bad2
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-02-07 18:15:28 -0700

    Update yapf

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ rm ~/.tmux.conf && ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 rm ~/.bash_profile && ln -s $(pwd)/.bash_profile ~/.bash_profile
 rm ~/.zshrc && ln -s $(pwd)/.zshrc ~/.zshrc
 rm ~/.condarc && ln -s $(pwd)/.condarc ~/.condarc
-rm ~/.config/yapf/style && ln -s $(pwd)/.yapf/style ~/.config/yapf/style
+rm ~/.config/yapf/style && ln -s $(pwd)/.yapf-style ~/.config/yapf/style
 rm ~/.config/flake8 && ln -s $(pwd)/.flake8 ~/.config/flake8
 rm ~/.config/.fzf.bash && ln -s $(pwd)/.fzf.bash ~/.config/.fzf.bash
 rm ~/.config/.fzf.zsh && ln -s $(pwd)/.fzf.zsh ~/.config/.fzf.zsh
