14d582b8b6e99f13bc7362ee2827b6eff5c294c8
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2019-06-13 11:15:46 -0600

    Update init.lua

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,3 +1,7 @@
+hyper = {"ctrl", "alt", "cmd"}
+hypershift = {"ctrl", "alt", "cmd", "shift"}
+cmdctrlshift = {"cmd", "ctrl", "shift"}
+
 hs.grid.setGrid('12x12') -- allows us to place on quarters, thirds and halves
 hs.grid.MARGINX = 0
 hs.grid.MARGINY = 0
@@ -7,12 +11,13 @@ hs.window.animationDuration = 0 -- disable animations
 -- Download and install http://www.hammerspoon.org/Spoons/SpoonInstall.html
 hs.loadSpoon("SpoonInstall")
 
-spoon.SpoonInstall:andUse("ReloadConfiguration")
-spoon.ReloadConfiguration:start()
-
-spoon.ReloadConfiguration:bindHotkeys({
-    reloadConfiguration = { { "ctrl", "alt", "cmd" }, "r" },
-})
+-- http://www.hammerspoon.org/Spoons/ReloadConfiguration.html
+spoon.SpoonInstall:andUse("ReloadConfiguration",
+               {
+                    config = { watch_paths = { os.getenv("HOME") .. "/GitRepos/dotfiles/.hammerspoon" } },
+                    hotkeys = { reloadConfiguration = { hyper, "r" } },
+                    start = true,
+               }
+)
 
 hs.alert.show("Config loaded!")
-
