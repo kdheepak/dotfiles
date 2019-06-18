23e0ae4a9ca42178a09fb861a57a2fded2a792d7
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2019-06-18 11:49:00 -0600

    Update files

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -476,37 +476,37 @@ hs.hotkey.bind("alt-shift", "`", switcher_previous, nil, switcher_previous) -- M
 --     end)
 -- end)
 
-border = nil
-
-function drawBorder()
-    if border then
-        border:delete()
-    end
-
-    local win = hs.window.focusedWindow()
-    if win == nil then return end
-
-    local f = win:frame()
-    local fx = f.x - 2
-    local fy = f.y - 2
-    local fw = f.w + 4
-    local fh = f.h + 4
-
-    border = hs.drawing.rectangle(hs.geometry.rect(fx, fy, fw, fh))
-    border:setStrokeWidth(3)
-    border:setStrokeColor({["red"]=1,["blue"]=0.4,["green"]=0.12,["alpha"]=1})
-    border:setRoundedRectRadii(5.0, 5.0)
-    border:setStroke(true):setFill(false)
-    border:setLevel("utility")
-    border:show()
-end
-
-drawBorder()
-
-windows = hs.window.filter.new(nil)
-windows:subscribe(hs.window.filter.windowFocused, function () drawBorder() end)
-windows:subscribe(hs.window.filter.windowUnfocused, function () drawBorder() end)
-windows:subscribe(hs.window.filter.windowMoved, function () drawBorder() end)
+-- border = nil
+--
+-- function drawBorder()
+--     if border then
+--         border:delete()
+--     end
+--
+--     local win = hs.window.focusedWindow()
+--     if win == nil then return end
+--
+--     local f = win:frame()
+--     local fx = f.x - 2
+--     local fy = f.y - 2
+--     local fw = f.w + 4
+--     local fh = f.h + 4
+--
+--     border = hs.drawing.rectangle(hs.geometry.rect(fx, fy, fw, fh))
+--     border:setStrokeWidth(3)
+--     border:setStrokeColor({["red"]=1,["blue"]=0.4,["green"]=0.12,["alpha"]=1})
+--     border:setRoundedRectRadii(5.0, 5.0)
+--     border:setStroke(true):setFill(false)
+--     border:setLevel("utility")
+--     border:show()
+-- end
+--
+-- drawBorder()
+--
+-- windows = hs.window.filter.new(nil)
+-- windows:subscribe(hs.window.filter.windowFocused, function () drawBorder() end)
+-- windows:subscribe(hs.window.filter.windowUnfocused, function () drawBorder() end)
+-- windows:subscribe(hs.window.filter.windowMoved, function () drawBorder() end)
 
 
 -------------------------------------------------------------------------------------
