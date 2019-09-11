ef623ead7c51d846c063336be91b4f61db9e6025
Dheepak Krishnamurthy <me@kdheepak.com>
2019-09-11 00:30:26 -0600

    Update files

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,7 +16,7 @@ rm ~/.zshenv; ln -s $(pwd)/.zshenv ~/.zshenv
 rm ~/.zshrc; ln -s $(pwd)/.zshrc ~/.zshrc
 rm ~/.condarc; ln -s $(pwd)/.condarc ~/.condarc
 rm ~/.config/yapf/style; ln -s $(pwd)/.yapf-style ~/.config/yapf/style
-rm ~/.config/alacritty/alacritty.yml; ln -s $(pwd)/alacritty ~/.config/alacritty/alacritty.yml
+rm ~/.config/alacritty/alacritty.yml; ln -s $(pwd)/alacritty.yml ~/.config/alacritty/alacritty.yml
 rm ~/.config/flake8; ln -s $(pwd)/.flake8 ~/.config/flake8
 rm ~/.config/pycodestyle; ln -s $(pwd)/.pycodestyle ~/.config/pycodestyle
 rm ~/.config/.fzf.bash; ln -s $(pwd)/.fzf.bash ~/.config/.fzf.bash
