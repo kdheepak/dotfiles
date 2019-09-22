ac2df56dd574302498e3e88041cbc215942db211
Dheepak Krishnamurthy <me@kdheepak.com>
2019-09-22 17:32:42 -0600

    Update files

--- .juliarc.jl
+++ .juliarc.jl
@@ -1,7 +1,8 @@
 using OhMyREPL
 
-USER = ENV["USER"]
+const USER = ENV["USER"]
 
+ENV["CONDA_JL_HOME"] = "/Users/$USER/miniconda3/envs/julia-py3-env"
 ENV["PYTHON"] = "/Users/$USER/miniconda3/envs/julia-py3-env/bin/python"
 ENV["JUPYTER"] = "/Users/$USER/miniconda3/envs/julia-py3-env/bin/jupyter"
 
