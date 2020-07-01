8fb44caab07aa6f82517c7cad2ca7ff80b1d86d3
Dheepak Krishnamurthy <me@kdheepak.com>
2020-07-01 09:41:05 -0600

    Update files

--- p10k.zsh
+++ p10k.zsh
@@ -341,7 +341,7 @@
   typeset -g POWERLEVEL9K_VCS_LOADING_BACKGROUND=8
 
   # Branch icon. Set this parameter to '\uF126 ' for the popular Powerline branch icon.
-  typeset -g POWERLEVEL9K_VCS_BRANCH_ICON=
+  typeset -g POWERLEVEL9K_VCS_BRANCH_ICON='\uF126 '
 
   # Untracked files icon. It's really a question mark, your font isn't broken.
   # Change the value of this parameter to show a different icon.
