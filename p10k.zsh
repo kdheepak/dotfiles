818be5883808734e4468910a6ffdd9811815fc05
Dheepak Krishnamurthy <me@kdheepak.com>
2020-07-12 13:35:01 -0600

    Update files

--- p10k.zsh
+++ p10k.zsh
@@ -1563,7 +1563,7 @@
   ####################################[ time: current time ]####################################
   # Current time color.
   # typeset -g POWERLEVEL9K_TIME_FOREGROUND=0
-  # typeset -g POWERLEVEL9K_TIME_BACKGROUND=7
+  typeset -g POWERLEVEL9K_TIME_BACKGROUND='#C0C0C0'
   # Format for the current time: 09:51:02. See `man 3 strftime`.
   typeset -g POWERLEVEL9K_TIME_FORMAT='%D{%H:%M:%S}'
   # If set to true, time will update when you hit enter. This way prompts for the past
