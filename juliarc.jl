cb8b3330ab3c12d32273bab416baa671421aa400
Dheepak Krishnamurthy <me@kdheepak.com>
2020-07-18 00:23:51 -0600

    Update files

--- juliarc.jl
+++ juliarc.jl
@@ -3,17 +3,15 @@ ENV["CONDA_JL_HOME"] = expanduser("~/miniconda3/envs/julia-py3-env")
 ENV["PYTHON"] = expanduser("~/miniconda3/envs/julia-py3-env/bin/python")
 ENV["JUPYTER"] = expanduser("~/miniconda3/envs/julia-py3-env/bin/jupyter")
 
-push!(LOAD_PATH, expanduser("~/GitRepos/Presentation.jl"))
-
-# atreplinit() do repl
-#     @async try
-#         sleep(0.1)
-#         @eval using Revise
-#         @async Revise.wait_steal_repl_backend()
-#     catch
-#         @warn("Could not load Revise.")
-#     end
-# end
+atreplinit() do repl
+    @async try
+        sleep(0.1)
+        @eval using Revise
+        @async Revise.wait_steal_repl_backend()
+    catch
+        @warn("Could not load Revise.")
+    end
+end
 
 using OhMyREPL
 
