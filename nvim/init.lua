169c03740c97e906ad89bb0a0a36d9be5e17a168
Dheepak Krishnamurthy <me@kdheepak.com>
2021-06-30 09:13:52 -0600

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,3 +15,12 @@ require 'kdheepak/autocommands'
 require 'kdheepak/vim'
 
 require 'kdheepak/color'
+
+vim.notify = function(msg, log_level, _)
+    if msg:match("exit code") then return end
+    if log_level == vim.log.levels.ERROR then
+        vim.api.nvim_err_writeln(msg)
+    else
+        vim.api.nvim_echo({{msg}}, true, {})
+    end
+end
