dd10ee9efd6131ad192f07fb001fda070fe0d539
Dheepak Krishnamurthy <me@kdheepak.com>
2021-07-05 23:10:34 -0600

    Update files

--- nvim/lua/kdheepak/debug.lua
+++ nvim/lua/kdheepak/debug.lua
@@ -22,3 +22,13 @@ dap.configurations.lua = {
 dap.adapters.nlua = function(callback, config)
   callback({ type = 'server', host = config.host, port = config.port })
 end
+
+local dap = require 'dap'
+dap.adapters.rust = {
+  type = 'executable',
+  attach = { pidProperty = 'pid', pidSelect = 'ask' },
+  command = 'lldb-vscode',
+  env = { LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = 'YES' },
+  name = 'lldb',
+}
+dap.configurations.rust = { { type = 'rust', name = 'Debug', request = 'launch' } }
