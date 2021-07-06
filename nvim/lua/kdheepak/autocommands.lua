1451bb8d30779f178887a216c3daa82b191f191f
Dheepak Krishnamurthy <me@kdheepak.com>
2021-07-06 12:39:18 -0600

    Update files

--- nvim/lua/kdheepak/autocommands.lua
+++ nvim/lua/kdheepak/autocommands.lua
@@ -51,31 +51,6 @@ augroup BgHighlight
     autocmd WinLeave * set nocursorline
 augroup END
 
-function! MathAndLiquid()
-    "" Define certain regions
-    " Block math. Look for "$$[anything]$$"
-    syn region math start=/\$\$/ end=/\$\$/
-    " inline math. Look for "$[not $][anything]$"
-    syn match math_block '\$[^$].\{-}\$'
-
-    " Liquid single line. Look for "{%[anything]%}"
-    syn match liquid '{%.*%}'
-    " Liquid multiline. Look for "{%[anything]%}[anything]{%[anything]%}"
-    syn region highlight_block start='{% highlight .*%}' end='{%.*%}'
-    " Fenced code blocks, used in GitHub Flavored Markdown (GFM)
-    syn region highlight_block start='```' end='```'
-
-    "" Actually highlight those regions.
-    hi link math Statement
-    hi link liquid Statement
-    hi link highlight_block Function
-    hi link math_block Function
-    hi! link markdownItalic Italic
-endfunction
-
-" Call everytime we open a Markdown file
-autocmd BufRead,BufNewFile,BufEnter *.md,*.markdown call MathAndLiquid()
-
 autocmd BufEnter * EnableStripWhitespaceOnSave
 let g:strip_whitespace_confirm=0
 let g:strip_only_modified_lines=1
