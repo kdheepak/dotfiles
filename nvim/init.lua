f8d501ce6810de4cfbba5c69fed69a3a7d39bcce
Dheepak Krishnamurthy <me@kdheepak.com>
2021-07-12 05:24:05 -0600

    Update files

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,15 @@ function _G.dump(...)
   print(unpack(objects))
 end
 
+-- Disable some unused built-in Neovim plugins
+vim.g.loaded_man = false
+vim.g.loaded_gzip = false
+vim.g.loaded_netrwPlugin = false
+vim.g.loaded_tarPlugin = false
+vim.g.loaded_zipPlugin = false
+vim.g.loaded_2html_plugin = false
+vim.g.loaded_remote_plugins = false
+
 require 'kdheepak/plugins'
 
 vim.notify = function(msg, log_level, _)
