e46095e4c76c12c59a2157d92299b5af64354a1b
Dheepak Krishnamurthy <me@kdheepak.com>
2021-08-01 23:55:27 -0600

    Update files

--- nvim/lua/kdheepak/plugins.lua
+++ nvim/lua/kdheepak/plugins.lua
@@ -455,7 +455,22 @@ packer.startup({
     use {
       'akinsho/nvim-toggleterm.lua',
       config = function()
-        require('toggleterm').setup {}
+        require('toggleterm').setup {
+          -- size can be a number or function which is passed the current terminal
+          open_mapping = [[<c-\>]],
+          hide_numbers = true, -- hide the number column in toggleterm buffers
+          shade_filetypes = {},
+          shade_terminals = true,
+          shading_factor = '<number>', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+          start_in_insert = true,
+          insert_mappings = true, -- whether or not the open mapping applies in insert mode
+          persist_size = true,
+          direction = 'float',
+          close_on_exit = true, -- close the terminal window when the process exits
+          shell = vim.o.shell, -- change the default shell
+          -- This field is only relevant if direction is set to 'float'
+          float_opts = { border = 'curved', winblend = 3, highlights = { border = 'Normal', background = 'Normal' } },
+        }
       end,
     }
     -- colorschemes
