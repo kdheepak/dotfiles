d4dfb57c7482da5d059ed2cc1deaa1e7824f6c21
Dheepak Krishnamurthy <me@kdheepak.com>
2021-08-19 17:48:17 -0600

    Update files

--- nvim/lua/kdheepak/lspconfig.lua
+++ nvim/lua/kdheepak/lspconfig.lua
@@ -1,24 +1,15 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.preselectSupport = true
+capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
+capabilities.textDocument.completion.completionItem.deprecatedSupport = true
+capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
+capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
+capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
 capabilities.textDocument.completion.completionItem.resolveSupport = {
   properties = { "documentation", "detail", "additionalTextEdits" },
 }
-
 capabilities.textDocument.completion.completionItem.documentationFormat = { "markdown" }
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.preselectSupport = true
-capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
-capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
-capabilities.textDocument.completion.completionItem.deprecatedSupport = true
-capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
-capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-  properties = {
-    "documentation",
-    "detail",
-    "additionalTextEdits",
-  },
-}
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
   virtual_text = false,
--- nvim/lua/kdheepak/plugins.lua
+++ nvim/lua/kdheepak/plugins.lua
@@ -344,14 +344,148 @@ packer.startup({
 
     use({ "mattn/emmet-vim", ft = { "html", "vue", "css" } })
 
+    -- Snippets
     use({
-      "lifepillar/vim-mucomplete",
+      "hrsh7th/nvim-compe",
+      requires = {
+        { "GoldsteinE/compe-latex-symbols" },
+        { "L3MON4D3/LuaSnip" },
+        { "onsails/lspkind-nvim" },
+      },
       config = function()
-        vim.cmd([[
-          set completeopt-=preview
-          set completeopt+=longest,menuone,noselect
-          let g:mucomplete#enable_auto_at_startup = 1
-        ]])
+        require("lspkind").init({
+          -- enables text annotations
+          --
+          -- default: true
+          with_text = true,
+
+          -- default symbol map
+          -- can be either 'default' (requires nerd-fonts font) or
+          -- 'codicons' for codicon preset (requires vscode-codicons font)
+          --
+          -- default: 'default'
+          preset = "codicons",
+
+          -- override preset symbols
+          --
+          -- default: {}
+          symbol_map = {
+            Text = "",
+            Method = "",
+            Function = "",
+            Constructor = "",
+            Field = "ﰠ",
+            Variable = "",
+            Class = "ﴯ",
+            Interface = "",
+            Module = "",
+            Property = "ﰠ",
+            Unit = "塞",
+            Value = "",
+            Enum = "",
+            Keyword = "",
+            Snippet = "",
+            Color = "",
+            File = "",
+            Reference = "",
+            Folder = "",
+            EnumMember = "",
+            Constant = "",
+            Struct = "פּ",
+            Event = "",
+            Operator = "",
+            TypeParameter = "",
+          },
+        })
+        -- Key mapping
+        function map(mode, key, result, opts)
+          opts = vim.tbl_extend("keep", opts or {}, {
+            noremap = true,
+            silent = true,
+            expr = false,
+          })
+          vim.api.nvim_set_keymap(mode, key, result, opts)
+        end
+
+        local nvim_compe = require("compe")
+        nvim_compe.setup({
+          enabled = true,
+          autocomplete = true,
+          documentation = false,
+          debug = false,
+          min_length = 1,
+          preselect = "always",
+          throttle_time = 80,
+          source_timeout = 200,
+          incomplete_delay = 400,
+          allow_prefix_unmatch = false,
+          source = {
+            path = true,
+            calc = true,
+            spell = true,
+            vsnip = true,
+            emoji = true,
+            buffer = true,
+            nvim_lsp = true,
+            nvim_lua = true,
+            latex_symbols = true,
+          },
+        })
+
+        local t = function(str)
+          return vim.api.nvim_replace_termcodes(str, true, true, true)
+        end
+
+        local luasnip = require("luasnip")
+
+        local check_back_space = function()
+          local col = vim.fn.col(".") - 1
+          if col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+            return true
+          else
+            return false
+          end
+        end
+
+        _G.tab_complete = function()
+          if vim.fn.pumvisible() == 1 then
+            return t("<C-n>")
+          elseif luasnip.expand_or_jumpable() then
+            return t("<Plug>luasnip-expand-or-jump")
+          elseif check_back_space() then
+            return t("<Tab>")
+          else
+            return vim.fn["compe#complete"]()
+          end
+        end
+
+        _G.s_tab_complete = function()
+          if vim.fn.pumvisible() == 1 then
+            return t("<C-p>")
+          elseif luasnip.jumpable(-1) then
+            return t("<Plug>luasnip-jump-prev")
+          else
+            return t("<S-Tab>")
+          end
+        end
+
+        _G.enter_complete = function()
+          if luasnip.choice_active() then
+            return t("<Plug>luasnip-next-choice")
+          end
+          return vim.fn["compe#confirm"](t("<CR>"))
+        end
+
+        map("i", "<CR>", "v:lua.enter_complete()", { expr = true })
+
+        map("i", "<Tab>", "v:lua.tab_complete()", { noremap = false, expr = true })
+        map("s", "<Tab>", "v:lua.tab_complete()", { noremap = false, expr = true })
+
+        map("i", "<S-Tab>", "v:lua.s_tab_complete()", { noremap = false, expr = true })
+        map("s", "<S-Tab>", "v:lua.s_tab_complete()", { noremap = false, expr = true })
+
+        map("i", "<C-u>", "compe#scroll({ 'delta': +4 })", { noremap = false, expr = true })
+        map("i", "<C-d>", "compe#scroll({ 'delta': -4 })", { noremap = false, expr = true })
       end,
     })
 
