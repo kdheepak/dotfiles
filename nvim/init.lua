d79e8abfafa3eb2c574212b880a0c3ca883f6813
Dheepak Krishnamurthy <me@kdheepak.com>
2021-09-02 03:58:52 -0600

    Simplify init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,3 @@
-vim.g.loaded_man = false
-vim.g.loaded_gzip = false
-vim.g.loaded_netrwPlugin = false
-vim.g.loaded_tarPlugin = false
-vim.g.loaded_zipPlugin = false
-vim.g.loaded_2html_plugin = false
-vim.g.loaded_remote_plugins = false
-
 require("kd/utils")
 
 require("kd/plugins")
