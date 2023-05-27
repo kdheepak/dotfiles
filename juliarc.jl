39bf85bd094c4e4a1cd40f3aa7f4c25022dc40fc
Dheepak Krishnamurthy <me@kdheepak.com>
2023-05-27 02:13:23 -0400

    chore: Update files ♻️

--- juliarc.jl
+++ juliarc.jl
@@ -6,74 +6,50 @@ else
   ENV["JULIA_EDITOR"] = "code"
 end
 
-# Based on https://github.com/fredrikekre/.dotfiles/blob/b3bc98513c960308409fe0f22ba7e232c385cdae/.julia/config/startup.jl
-if Base.isinteractive() &&
-   (local REPL = get(Base.loaded_modules, Base.PkgId(Base.UUID("3fa0cd96-eef1-5676-8a61-b3b8758bbffb"), "REPL"), nothing); REPL !== nothing)
-
-  # Exit Julia with :q, restart with :r
-  pushfirst!(REPL.repl_ast_transforms, function (ast::Union{Expr,Nothing})
-    function toplevel_quotenode(ast, s)
-      return (Meta.isexpr(ast, :toplevel, 2) && ast.args[2] === QuoteNode(s)) ||
-             (Meta.isexpr(ast, :toplevel) && any(x -> toplevel_quotenode(x, s), ast.args))
-    end
-    if toplevel_quotenode(ast, :q)
-      exit()
-    elseif toplevel_quotenode(ast, :r)
-      argv = Base.julia_cmd().exec
-      opts = Base.JLOptions()
-      if opts.project != C_NULL
-        push!(argv, "--project=$(unsafe_string(opts.project))")
-      end
-      if opts.nthreads != 0
-        push!(argv, "--threads=$(opts.nthreads)")
-      end
-      # @ccall execv(argv[1]::Cstring, argv::Ref{Cstring})::Cint
-      ccall(:execv, Cint, (Cstring, Ref{Cstring}), argv[1], argv)
-    end
-    return ast
-  end)
-
-  # Automatically load tooling on demand:
-  # - BenchmarkTools.jl when encountering @btime or @benchmark
-  # - Cthulhu.jl when encountering @descend(_code_(typed|warntype))
-  # - Debugger.jl when encountering @enter or @run
-  # - Profile.jl when encountering @profile
-  # - ProfileView.jl when encountering @profview
-  local tooling_dict = Dict{Symbol,Vector{Symbol}}(
-    :BenchmarkTools => Symbol.(["@btime", "@benchmark"]),
-    :Cthulhu => Symbol.(["@descend", "@descend_code_typed", "@descend_code_warntype"]),
-    :Debugger => Symbol.(["@enter", "@run"]),
-    :Profile => Symbol.(["@profile"]),
-    :ProfileView => Symbol.(["@profview"]),
-  )
-  pushfirst!(REPL.repl_ast_transforms, function (ast::Union{Expr,Nothing})
-    function contains_macro(ast, m)
-      return ast isa Expr && (
-        (Meta.isexpr(ast, :macrocall) && ast.args[1] === m) ||
-        any(x -> contains_macro(x, m), ast.args)
-      )
-    end
-    for (mod, macros) in tooling_dict
-      if any(contains_macro(ast, s) for s in macros) && !isdefined(Main, mod)
-        @info "Loading $mod ..."
-        try
-          Core.eval(Main, :(using $mod))
-        catch err
-          @info "Failed to automatically load $mod" exception = err
-        end
-      end
-    end
-    return ast
-  end)
-
+import Pkg
+if isdefined(Pkg.Resolve, :CONFLICT_COLORS)
+  append!(Pkg.Resolve.CONFLICT_COLORS, [21:51; 55:119; 124:142; 160:184; 196:220])
 end
 
-# if isfile("Project.toml") && isfile("Manifest.toml")
-#  using Pkg
-#  Pkg.activate(".")
-# end
-
 import Pkg
+if isfile("Project.toml") && isfile("Manifest.toml")
+  Pkg.activate(".")
+end
+
+# Setup OhMyREPL and Revise
+import Pkg
+let
+  pkgs = ["Revise", "OhMyREPL", "BenchmarkTools", "Crayons"]
+  for pkg in pkgs
+    if Base.find_package(pkg) === nothing
+      Pkg.add(pkg)
+    end
+  end
+end
+
+"""
+    @subprocess ex
+    @subprocess ex wait=false
+
+Execute `ex` in a subprocess that mimics the current process configuration.
+Returns the constructed `Process`.
+See [`Base.julia_cmd`](@ref) for the subprocess configuration forwarding that is used.
+
+```julia-repl
+julia> println("hello from $(Base.getpid())")
+hello from 35640
+
+julia> @subprocess println("hello from $(Base.getpid())")
+hello from 43056
+Process(`/home/user/julia/julia -Cnative -J/home/user/julia/usr/lib/julia/sys.dylib -g1 -e 'println("hello from $(Base.getpid())")'`, ProcessExited(0))
+```
+"""
+macro subprocess(ex, wait=true)
+  quote
+    local ex_str = $(esc(sprint(Base.show_unquoted, ex)))
+    run(`$(Base.julia_cmd()) -e "$(ex_str)"`, wait=$(wait))
+  end
+end
 
 Base.atreplinit() do repl
   @eval begin
@@ -81,7 +57,7 @@ Base.atreplinit() do repl
     # JuliaSyntax.enable_in_core!(true)
     # import Term
     # Term.install_term_stacktrace()
-    @async @eval using Revise
+    @eval using Revise
     @async @eval using BenchmarkTools
     @async @eval using Infiltrator
     @async @eval using ReverseStackTraces
@@ -90,6 +66,72 @@ Base.atreplinit() do repl
     OMR.enable_pass!("RainbowBrackets", false)
     OMR.Passes.BracketHighlighter.setcrayon!(C.Crayon(foreground=:blue))
     # OMR.input_prompt!("($(Pkg.REPLMode.projname(Pkg.Types.find_project_file()))) julia> ")
+
+
+    # Based on https://github.com/fredrikekre/.dotfiles/blob/b3bc98513c960308409fe0f22ba7e232c385cdae/.julia/config/startup.jl
+    if Base.isinteractive() &&
+       (local REPL = get(Base.loaded_modules, Base.PkgId(Base.UUID("3fa0cd96-eef1-5676-8a61-b3b8758bbffb"), "REPL"), nothing); REPL !== nothing)
+
+      # Exit Julia with :q, restart with :r
+      pushfirst!(REPL.repl_ast_transforms, function (ast::Union{Expr,Nothing})
+        function toplevel_quotenode(ast, s)
+          return (Meta.isexpr(ast, :toplevel, 2) && ast.args[2] === QuoteNode(s)) ||
+                 (Meta.isexpr(ast, :toplevel) && any(x -> toplevel_quotenode(x, s), ast.args))
+        end
+        if toplevel_quotenode(ast, :q)
+          exit()
+        elseif toplevel_quotenode(ast, :r)
+          argv = Base.julia_cmd().exec
+          opts = Base.JLOptions()
+          if opts.project != C_NULL
+            push!(argv, "--project=$(unsafe_string(opts.project))")
+          end
+          if opts.nthreads != 0
+            push!(argv, "--threads=$(opts.nthreads)")
+          end
+          # @ccall execv(argv[1]::Cstring, argv::Ref{Cstring})::Cint
+          ccall(:execv, Cint, (Cstring, Ref{Cstring}), argv[1], argv)
+        end
+        return ast
+      end)
+
+      # Automatically load tooling on demand:
+      # - BenchmarkTools.jl when encountering @btime or @benchmark
+      # - Cthulhu.jl when encountering @descend(_code_(typed|warntype))
+      # - Debugger.jl when encountering @enter or @run
+      # - Profile.jl when encountering @profile
+      # - ProfileView.jl when encountering @profview
+      local tooling_dict = Dict{Symbol,Vector{Symbol}}(
+        :BenchmarkTools => Symbol.(["@btime", "@benchmark"]),
+        :Cthulhu => Symbol.(["@descend", "@descend_code_typed", "@descend_code_warntype"]),
+        :Debugger => Symbol.(["@enter", "@run"]),
+        :Profile => Symbol.(["@profile"]),
+        :ProfileView => Symbol.(["@profview"]),
+      )
+      pushfirst!(REPL.repl_ast_transforms, function (ast::Union{Expr,Nothing})
+        function contains_macro(ast, m)
+          return ast isa Expr && (
+            (Meta.isexpr(ast, :macrocall) && ast.args[1] === m) ||
+            any(x -> contains_macro(x, m), ast.args)
+          )
+        end
+        for (mod, macros) in tooling_dict
+          if any(contains_macro(ast, s) for s in macros) && !isdefined(Main, mod)
+            @info "Loading $mod ..."
+            try
+              Core.eval(Main, :(using $mod))
+            catch err
+              @info "Failed to automatically load $mod" exception = err
+            end
+          end
+        end
+        return ast
+      end)
+
+    end
+
+
+
   end
 end
 
