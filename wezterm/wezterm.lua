4ea6cd9ba5f60aea7fd3802027758ae01bd03eb9
Dheepak Krishnamurthy <noreply@kdheepak.com>
2024-07-28 00:19:28 -0400

    chore: Update files

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,7 +10,73 @@ local function iosevka(weight, style)
   return wezterm.font("IosevkaFireTerm Nerd Font Mono", { weight = weight, style = style })
 end
 
+wezterm.on("gui-startup", function()
+  local tab, pane, window = wezterm.mux.spawn_window({})
+  window:gui_window():maximize()
+  window:gui_window():focus()
+end)
+
+-- if you are *NOT* lazy-loading smart-splits.nvim (recommended)
+local function is_vim(pane)
+  -- this is set by the plugin, and unset on ExitPre in Neovim
+  return pane:get_user_vars().IS_NVIM == "true"
+end
+
+-- if you *ARE* lazy-loading smart-splits.nvim (not recommended)
+-- you have to use this instead, but note that this will not work
+-- in all cases (e.g. over an SSH connection). Also note that
+-- `pane:get_foreground_process_name()` can have high and highly variable
+-- latency, so the other implementation of `is_vim()` will be more
+-- performant as well.
+local function is_vim(pane)
+  -- This gsub is equivalent to POSIX basename(3)
+  -- Given "/foo/bar" returns "bar"
+  -- Given "c:\\foo\\bar" returns "bar"
+  local process_name = string.gsub(pane:get_foreground_process_name(), "(.*[/\\])(.*)", "%2")
+  return process_name == "nvim" or process_name == "vim"
+end
+
+local direction_keys = {
+  h = "Left",
+  j = "Down",
+  k = "Up",
+  l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+  return {
+    key = key,
+    mods = resize_or_move == "resize" and "META" or "CTRL",
+    action = wezterm.action_callback(function(win, pane)
+      if is_vim(pane) then
+        -- pass the keys through to vim/nvim
+        win:perform_action({
+          SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+        }, pane)
+      else
+        if resize_or_move == "resize" then
+          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+        else
+          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+        end
+      end
+    end),
+  }
+end
+
 config = {
+  keys = {
+    -- move between split panes
+    split_nav("move", "h"),
+    split_nav("move", "j"),
+    split_nav("move", "k"),
+    split_nav("move", "l"),
+    -- resize panes
+    split_nav("resize", "h"),
+    split_nav("resize", "j"),
+    split_nav("resize", "k"),
+    split_nav("resize", "l"),
+  },
   color_scheme = "rose-pine",
   default_cursor_style = "BlinkingBar",
   automatically_reload_config = true,
@@ -19,8 +85,9 @@ config = {
   window_decorations = "RESIZE",
   check_for_updates = false,
   use_fancy_tab_bar = false,
-  default_prog = { "zellij" },
+  default_prog = { "/bin/zsh", "-l", "-c", "~/.local/share/mise/shims/zellij" },
   tab_bar_at_bottom = false,
+  audible_bell = "Disabled",
   font_size = 12,
   font_rules = {
     {
@@ -56,8 +123,8 @@ config = {
   },
   enable_tab_bar = false,
   window_padding = {
-    left = 3,
-    right = 3,
+    left = 0,
+    right = 0,
     top = 0,
     bottom = 0,
   },
