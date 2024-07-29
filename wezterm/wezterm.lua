f439adf8bdda5a28ea394fd820314046456040de
Dheepak Krishnamurthy <noreply@kdheepak.com>
2024-07-29 04:06:17 -0400

    chore: Update files

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,8 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+
+local act = wezterm.action
+
 local config = {}
 
 if wezterm.config_builder then
@@ -113,6 +116,38 @@ config.hyperlink_rules = {
   },
 }
 
+wezterm.on("trigger-vim-with-scrollback", function(window, pane)
+  -- Retrieve the text from the pane
+  local text = pane:get_lines_as_text(pane:get_dimensions().scrollback_rows)
+
+  -- Create a temporary file to pass to vim
+  local name = os.tmpname()
+  local f = io.open(name, "w+")
+  if f == nil then
+    return
+  end
+  f:write(text)
+  f:flush()
+  f:close()
+
+  -- Open a new window running vim and tell it to open the file
+  window:perform_action(
+    act.SpawnCommandInNewWindow({
+      args = { "vim", name },
+    }),
+    pane
+  )
+
+  -- Wait "enough" time for vim to read the file before we remove it.
+  -- The window creation and process spawn are asynchronous wrt. running
+  -- this script and are not awaitable, so we just pick a number.
+  --
+  -- Note: We don't strictly need to remove this file, but it is nice
+  -- to avoid cluttering up the temporary directory.
+  wezterm.sleep_ms(1000)
+  os.remove(name)
+end)
+
 local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
 bar.apply_to_config(config, {
   enabled_modules = {
@@ -120,8 +155,6 @@ bar.apply_to_config(config, {
     hostname = false,
   },
 })
-
-local act = wezterm.action
 
 config.leader = { key = "`", timeout_milliseconds = 1000 }
 
@@ -189,6 +222,11 @@ config.keys = {
         end),
       },
     }),
+  },
+  {
+    key = "e",
+    mods = "LEADER",
+    action = act.EmitEvent("trigger-vim-with-scrollback"),
   },
   { key = "UpArrow", mods = "SHIFT", action = act.ScrollToPrompt(-1) },
   { key = "DownArrow", mods = "SHIFT", action = act.ScrollToPrompt(1) },
