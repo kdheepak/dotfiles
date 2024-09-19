6d3a709da932f0de1bbc32ef5322e60cedf0c3e9
Dheepak Krishnamurthy <noreply@kdheepak.com>
2024-09-19 09:24:14 -0400

    chore: Update files

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -36,12 +36,44 @@ config.inactive_pane_hsb = {
   brightness = 1,
 }
 
+local color = {
+  rosewater = "#f5e0dc",
+  flamingo = "#f2cdcd",
+  pink = "#f5c2e7",
+  mauve = "#cba6f7",
+  red = "#f38ba8",
+  maroon = "#eba0ac",
+  peach = "#fab387",
+  yellow = "#f9e2af",
+  green = "#a6e3a1",
+  teal = "#94e2d5",
+  sky = "#89dceb",
+  sapphire = "#74c7ec",
+  blue = "#89b4fa",
+  lavender = "#b4befe",
+  text = "#cdd6f4",
+  subtext1 = "#bac2de",
+  subtext0 = "#a6adc8",
+  overlay2 = "#9399b2",
+  overlay1 = "#7f849c",
+  overlay0 = "#6c7086",
+  surface2 = "#585b70",
+  surface1 = "#45475a",
+  surface0 = "#313244",
+  base = "#1e1e2e",
+  mantle = "#181825",
+  crust = "#11111b",
+}
+
 config.color_scheme = "rose-pine"
 config.colors = {
-  -- the foreground color of selected text
-  selection_fg = "black",
-  -- the background color of selected text
-  selection_bg = "white",
+  selection_fg = color.text,
+  selection_bg = color.surface2,
+  compose_cursor = color.flamingo,
+  scrollbar_thumb = color.surface2,
+  cursor_fg = color.black,
+  cursor_bg = color.red,
+  cursor_border = color.rosewater,
 }
 config.default_cursor_style = "BlinkingBar"
 config.automatically_reload_config = true
