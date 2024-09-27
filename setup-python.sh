a624e305697c97f1860622e48de724cc919af4f9
Dheepak Krishnamurthy <noreply@kdheepak.com>
2024-09-27 09:34:41 -0400

    chore: Update files

--- setup-python.sh
+++ setup-python.sh
@@ -359,7 +359,7 @@ main() {
     uv_tool_install pre-commit
     uv_tool_install pylint
     uv_tool_install httpie
-    mamba_install bat delta direnv fzf gh git-lfs ipython jupyter neovim nodejs pandoc ripgrep starship unrar python-localvenv-kernel jupyterlab_execute_time jupyterlab-lsp python-lsp-server jupytext
+    mamba_install bat delta direnv fzf gh git-lfs ipython jupyter neovim nodejs pandoc ripgrep starship unrar python-localvenv-kernel jupyterlab_execute_time jupyterlab-lsp python-lsp-server jupytext ruff
 
     if [[ -z $ON_WINDOWS ]]; then
         # if ON_WINDOWS is not set
