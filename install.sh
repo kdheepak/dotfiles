34a135b8525332865b71ddadd9ffa13e8a88ddce
Dheepak Krishnamurthy <1813121+kdheepak@users.noreply.github.com>
2024-11-20 02:19:31 -0500

    chore: Update files

--- install.sh
+++ install.sh
@@ -2,10 +2,6 @@
 echo "Installing requirements"
 
 brew install neovim --HEAD
-brew install exa
-brew install ripgrep
-brew install bat
-brew install cmake
 brew install tmux
 brew install pkg-config
 brew install fontconfig
@@ -17,6 +13,6 @@ brew tap homebrew/cask-fonts
 brew cask install font-hasklig-nerd-font
 brew cask install font-firacode-nerd-font
 
-brew install coreutils binutils diffutils findutils gawk gnu-indent gnu-sed gnu-tar gnu-which gnutls grep gzip watch wdiff wget gdb gpatch make git less openssh rsync unzip
+brew install coreutils binutils diffutils findutils gawk gnu-indent gnu-sed gnu-tar gnu-which gnutls grep gzip watch wdiff wget gdb gpatch make git less openssh unzip sevenzip
 
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
