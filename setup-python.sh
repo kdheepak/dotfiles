249c36ec35b3da074c22ad4ca7e367d7d1315a94
Dheepak Krishnamurthy <1813121+kdheepak@users.noreply.github.com>
2025-01-24 08:21:12 -0500

    chore: Update files

--- setup-python.sh
+++ setup-python.sh
@@ -457,7 +457,7 @@ download_dependencies() {
     uv_tool_install httpie
     uv_tool_install black
 
-    mamba_install eza bat delta direnv fzf gh git-lfs ipython jupyter neovim nodejs pandoc ripgrep starship unrar python-localvenv-kernel jupyterlab jupyterlab_execute_time jupyterlab-lsp python-lsp-server jupytext ruff cmake panel hvplot holoviews watchfiles param matplotlib numpy pandas "ibis-framework[duckdb,geospatial]" lonboard just tokei
+    mamba_install eza bat delta direnv fzf gh git-lfs ipython jupyter neovim nodejs pandoc ripgrep starship unrar python-localvenv-kernel jupyterlab jupyterlab_execute_time jupyterlab-lsp python-lsp-server jupytext ruff cmake panel hvplot holoviews watchfiles param matplotlib numpy pandas "ibis-framework[duckdb,geospatial]" lonboard just tokei git-cliff
 
     if [[ -z $ON_WINDOWS ]]; then
         # if ON_WINDOWS is not set
