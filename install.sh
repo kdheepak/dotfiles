34e7bb273daf51d26964e288a7ff9ee36767b289
Dheepak Krishnamurthy <1813121+kdheepak@users.noreply.github.com>
2025-08-10 07:26:06 -0400

    chore(install): remove unused brew package installations

--- install.sh
+++ install.sh
@@ -13,7 +13,4 @@ brew cask install font-firacode-nerd-font
 
 brew install coreutils binutils diffutils findutils gawk gnu-indent gnu-sed gnu-tar gnu-which gnutls grep gzip watch wdiff wget gdb gpatch make git less openssh unzip sevenzip
 
-brew install delta
-brew install mods
-
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
